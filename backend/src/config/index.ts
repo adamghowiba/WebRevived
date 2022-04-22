79aad7f393bc680230b0558b71d4a82ba55e221e
Adam <adamware99@hotmail.com>
2022-04-22 13:09:48 +0400

    removed dotenv error

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,11 +2,12 @@ import dotenv from 'dotenv';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const envFound = dotenv.config();
+/* Commeted out due to issues causing contianer to crash */
+// const envFound = dotenv.config();
 
-if (envFound.error) {
-	throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
+// if (envFound.error) {
+// 	throw new Error("⚠️  Couldn't find .env file  ⚠️");
+// }
 
 export const isProduction = process.env.NODE_ENV === 'production';
 export const isDevelopment = process.env.NODE_ENV === 'development';
