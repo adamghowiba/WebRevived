1bdd909d4fed409a8f7483e6a4958c72f1d1ef9b
Adam <adamware99@hotmail.com>
2022-04-22 11:35:54 +0400

    test: Test build command

--- backend/src/api/routes/index.ts
+++ backend/src/api/routes/index.ts
@@ -5,9 +5,12 @@ const router = Router();
 /* GET home page. */
 router.get('/hello', (req: Request, res: Response) => {
 
-	res.json('Were live baby!');
+	res.json('The latest, of the latest test. Take 4000.');
 });
+
 
 export default router;
 
 // 040133161491.dkr.ecr.us-east-1.amazonaws.com/webrevived-backend
+
+/* Last push: April 22, 2022, 10:54:42 (UTC+04) */
