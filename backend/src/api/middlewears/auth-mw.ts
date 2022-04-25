ff9369cb0d101e918be86205c55f9547fbcf047f
Adam <adamware99@hotmail.com>
2022-04-26 00:00:18 +0400

    fix: auth mw unresolved error, docker file ending in slash.

--- backend/src/api/middlewears/auth-mw.ts
+++ backend/src/api/middlewears/auth-mw.ts
@@ -13,28 +13,29 @@ import { Request, Response, NextFunction } from 'express';
 export const requireRole = (role: UserRole, allowAbove = false) => {
 	return (req: Request, res: Response, next: NextFunction) => {
 		/* TODO Return before next; */
-		if (!req.session?.user || !req.session?.user?.role) next(new ApiError('Unauthrized, you must be logged.'));
-		const sessionRole = req.session.user.role;
+		if (!req.session?.user || !req.session?.user?.role) return next(new ApiError('Unauthrized, you must be logged.'));
+		const sessionRole = req.session?.user?.role;
 
 		if (allowAbove ? !getAboveRoles(role).includes(sessionRole) : sessionRole !== role)
-			next(new ApiError("Unauthrized, you don't have sufficient permissions"));
+			return next(new ApiError("Unauthrized, you don't have sufficient permissions"));
 
-		next();
+		return next();
 	};
 };
 
 export const requireRoles = (...role: UserRole[]) => {
 	return (req: Request, res: Response, next: NextFunction) => {
-		if (!req.session?.user || !req.session?.user?.role) next(new ApiError('Unauthrized, you must be logged.'));
+		if (!req.session?.user || !req.session?.user?.role) return next(new ApiError('Unauthrized, you must be logged.'));
 
-		if (!role.includes(req.session.user.role)) next(new ApiError("Unauthrized, you don't have sufficient permissions"));
+		if (!role.includes(req.session.user.role))
+			return next(new ApiError("Unauthrized, you don't have sufficient permissions"));
 
-		next();
+		return next();
 	};
 };
 
 export default (req: Request, res: Response, next: NextFunction) => {
-	if (!req.session.user) next(new ApiError('Unauthorized, you must be logged in.'));
+	if (!req.session.user) return next(new ApiError('Unauthorized, you must be logged in.'));
 
-	next();
+	return next();
 };
