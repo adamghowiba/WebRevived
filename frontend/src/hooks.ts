ba7ac39266895ddcbcbc23faab2dbe24a79910ec
Adam <adamware99@hotmail.com>
2022-04-27 00:31:44 +0400

    fix: remove api request causing docker build to break

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -2,10 +2,10 @@ import authApi from '$lib/api/auth-api';
 import type { GetSession, Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const { response: userResponse, result } = await authApi.getUserInformation(
-		event.request.headers.get('cookie')
-	);
-	if (userResponse.ok) event.locals.user = result;
+	// const { response: userResponse, result } = await authApi.getUserInformation(
+	// 	event.request.headers.get('cookie')
+	// );
+	// if (userResponse.ok) event.locals.user = result;
 
 	const response = await resolve(event);
 	return response;
