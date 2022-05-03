a4502e79e5a0a13260d66858d6908f64ac8e4d27
Adam <adamware99@hotmail.com>
2022-05-03 05:42:31 +0400

    fix: (website) remove bad links

--- apps/website/src/routes/index.svelte
+++ apps/website/src/routes/index.svelte
@@ -15,7 +15,7 @@
 		// title: 'We operate at the intersection of science and style.',
 		headings: ['We operate at', 'the intersection', 'of science and', 'style.'],
 		desc: 'We help you unlock your unique growth loops and put them into action with frameworks and strategies that drive rapid growth and maximize revenue potential.',
-		button: { text: 'See our approach', href: '/approach' }
+		button: { text: 'See our approach', href: '/' }
 	}}
 	icon="funnel"
 />
@@ -30,7 +30,7 @@
 			// title: 'The missing piece of your marketing puzzle.',
 			headings: ['The missing', 'peice of your', 'marketing pizzle'],
 			desc: "Armed with diverse perspectives and experiences, our high-energy teams unite to develop world-class digital experiences that grow companies. From brand strategy to web development to digital marketing, beautiful design isn't just what we do; it's our life’s work.",
-			button: { text: 'See our approach', href: '/learn about us' }
+			button: { text: 'See our approach', href: '/' }
 		}}
 		icon="puzzle"
 	/>
@@ -40,7 +40,7 @@
 			// title: 'No Fluff. High impact. No penny wasted.',
 			headings: ['No Fluff. High', 'Impact. No penny wasted'],
 			desc: 'We are hyper-focused on increasing your revenue. We leverage learnings from past projects & experiments with successful brands and startups to refine the growth playbook we create for your business.',
-			button: { text: 'See our services', href: '/services' }
+			button: { text: 'See our services', href: '/' }
 		}}
 		icon="barChart"
 		contentLocation="right"
