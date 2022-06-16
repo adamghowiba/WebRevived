9db631d1af207eab31902ce503576e94b2c5e95f
Adam <adamware99@hotmail.com>
2022-06-17 00:44:48 +0400

    [patch] changes npm version for pipeline testing

--- apps/backend/src/api/routes/index.ts
+++ apps/backend/src/api/routes/index.ts
@@ -7,13 +7,13 @@ router.get('/api/hello', (req: Request, res: Response) => {
 	res.json('Latest change with first auto deploy');
 });
 
-/* GET home page. */
-router.get('/api', (req: Request, res: Response) => {
-	res.status(200).json(`Web Revived Core API v0.2.0 BETA [npm ${process.env.npm_package_version}] - api route`);
+router.get('/', (req: Request, res: Response) => {
+	res.status(200).json(`Web Revived Core API BETA ${process.env.npm_package_version}`);
 });
 
-router.get('/', (req: Request, res: Response) => {
-	res.status(200).json(`Web Revived Core API v0.2.0 BETA [npm ${process.env.npm_package_version}]`);
+/* GET home page. */
+router.get('/api', (req: Request, res: Response) => {
+	res.status(200).json(`Web Revived Core API BETA ${process.env.npm_package_version}]`);
 });
 
 export default router;
